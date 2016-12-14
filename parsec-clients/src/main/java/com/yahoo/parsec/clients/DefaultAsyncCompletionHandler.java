66f19fbe3677eca4dc60616868ef3fcd85f52764
Yam Lin <yamlin@yahoo-inc.com>
2016-12-14 11:26:40 +0800

    Add DefaultAsyncCompletionHandler

---
+++ parsec-clients/src/main/java/com/yahoo/parsec/clients/DefaultAsyncCompletionHandler.java
@@ -0,0 +1,58 @@
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.parsec.clients;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.Response;
+
+import javax.xml.ws.http.HTTPException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This handler is used to map the response body to object T.
+ *
+ * @author yamlin
+ */
+public class DefaultAsyncCompletionHandler<T> extends AsyncCompletionHandler<T> {
+    private final ObjectMapper objectMapper;
+    private final Class<T> tClass;
+    private final Set<Integer> expectedStatusCodes;
+
+    public DefaultAsyncCompletionHandler(Class<T> tClass, Set<Integer> expected, ObjectMapper o) {
+        this.objectMapper = o;
+        this.tClass = tClass;
+
+        if (expected == null || expected.isEmpty()) {
+            this.expectedStatusCodes = new HashSet<>();
+            this.expectedStatusCodes.add(200);
+        } else {
+            this.expectedStatusCodes = expected;
+        }
+    }
+
+    public DefaultAsyncCompletionHandler(Class<T> tClass, Set<Integer> expected) {
+        this(tClass, expected, new ObjectMapper());
+    }
+
+    public DefaultAsyncCompletionHandler(Class<T> tClass, ObjectMapper o) {
+        this(tClass, null, o);
+    }
+
+    public DefaultAsyncCompletionHandler(Class<T> tClass) {
+        this(tClass, null, new ObjectMapper());
+    }
+
+    @Override
+    public T onCompleted(Response response) throws Exception {
+        if (expectedStatusCodes.contains(response.getStatusCode())) {
+            if (response.hasResponseBody()) {
+                return objectMapper.readValue(response.getResponseBody(), tClass);
+            }
+            return null;
+        } else {
+            throw new HTTPException(response.getStatusCode());
+        }
+    }
+}
---
+++ parsec-clients/src/test/java/com/yahoo/parsec/clients/DefaultAsyncCompletionHandlerTest.java
@@ -0,0 +1,79 @@
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
+package com.yahoo.parsec.clients;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ning.http.client.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.xml.ws.http.HTTPException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by yamlin on 2016/12/20.
+ */
+public class DefaultAsyncCompletionHandlerTest {
+    private Response mockResponse;
+    private ObjectMapper mockObjectMapper;
+
+    @BeforeMethod
+    @SuppressWarnings("unchecked")
+    public void setUp() throws Exception {
+        mockResponse = mock(Response.class);
+        mockObjectMapper = mock(ObjectMapper.class);
+    }
+
+    @Test
+    public void testOnCompletion() throws Exception {
+        when(mockResponse.getStatusCode()).thenReturn(200);
+        when(mockResponse.hasResponseBody()).thenReturn(true);
+        when(mockResponse.getResponseBody()).thenReturn("true");
+
+        DefaultAsyncCompletionHandler<String> handler =
+                new DefaultAsyncCompletionHandler<>(String.class);
+        Assert.assertEquals(handler.onCompleted(mockResponse), "true");
+    }
+
+    @Test
+    public void testOnCompletionWithProvidedMapper() throws Exception {
+        when(mockResponse.getStatusCode()).thenReturn(200);
+        when(mockResponse.hasResponseBody()).thenReturn(true);
+        when(mockResponse.getResponseBody()).thenReturn("true");
+        when(mockObjectMapper.readValue(anyString(), any(Class.class))).thenReturn("true");
+
+        DefaultAsyncCompletionHandler<String> handler =
+                new DefaultAsyncCompletionHandler<>(String.class, mockObjectMapper);
+        Assert.assertEquals(handler.onCompleted(mockResponse), "true");
+    }
+
+    @Test
+    public void testNoResponseBody() throws Exception {
+        Set<Integer> expected = new HashSet<>();
+        expected.add(201);
+        when(mockResponse.getStatusCode()).thenReturn(201);
+        when(mockResponse.hasResponseBody()).thenReturn(false);
+
+        DefaultAsyncCompletionHandler<String> handler =
+                new DefaultAsyncCompletionHandler<>(String.class, expected);
+        Assert.assertEquals(handler.onCompleted(mockResponse), null);
+    }
+
+    @Test(expectedExceptions = HTTPException.class)
+    public void testWrongStatusCodeFail() throws Exception {
+        Set<Integer> expected = new HashSet<>();
+        expected.add(200);
+        when(mockResponse.getStatusCode()).thenReturn(500);
+
+        DefaultAsyncCompletionHandler<String> handler =
+                new DefaultAsyncCompletionHandler<>(String.class, expected);
+        handler.onCompleted(mockResponse);
+    }
+}
