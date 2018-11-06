4302eb19ad8562568ec61e1033af54ea0f4bdcf7
Hans Hsieh <cahsieh@oath.com>
2018-11-06 10:41:50 +0800

    Add license lines

--- parsec-clients/src/test/java/com/yahoo/parsec/clients/StandardNameResolverTest.java
+++ parsec-clients/src/test/java/com/yahoo/parsec/clients/StandardNameResolverTest.java
@@ -1,6 +1,7 @@
-package com.yahoo.parsec.clients;
+// Copyright 2016 Yahoo Inc.
+// Licensed under the terms of the Apache license. Please see LICENSE.md file distributed with this work for terms.
 
-import com.ning.http.client.NameResolver;
+package com.yahoo.parsec.clients;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
